2db3c575a3c107a11fab902f87aa63296cf77061
Louis Pilfold <louis@lpil.uk>
2017-09-05 20:51:42 +0100

    Parser exploration

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,112 @@
-module Lib
-  ( greet
-  ) where
+module Lib where
 
+import Control.Arrow ((>>>))
+import Data.String.Here
+import qualified Language.PureScript as PS
 import Protolude
+import qualified Text.Parsec as P (ParseError)
 
 greet :: Text -> Text
 greet name = "Hello, " <> name <> "!"
+
+tokenize :: Text -> Either P.ParseError [Text]
+tokenize = PS.lex "file.purs" >>> fmap (map show)
+
+parse :: Text -> Either P.ParseError PS.Module
+parse source =
+  PS.lex "file.purs" source >>= PS.runTokenParser "file.purs" PS.parseModule
+
+source :: Text
+source =
+  [here|
+module Main where
+
+import Control.Monad.Eff.Console
+
+main = log "Hello, World!"
+|]
+-- Module
+--   (SourceSpan
+--    { spanName = "file.purs"
+--    , spanStart = SourcePos {sourcePosLine = 1, sourcePosColumn = 1}
+--    , spanEnd = SourcePos {sourcePosLine = 5, sourcePosColumn = 12}
+--    })
+--   []
+--   (ModuleName [ProperName {runProperName = "Main"}])
+--   [ ImportDeclaration
+--       ( SourceSpan
+--         { spanName = "file.purs"
+--         , spanStart = SourcePos {sourcePosLine = 3, sourcePosColumn = 1}
+--         , spanEnd = SourcePos {sourcePosLine = 3, sourcePosColumn = 33}
+--         }
+--       , [])
+--       (ModuleName
+--          [ ProperName {runProperName = "Control"}
+--          , ProperName {runProperName = "Monad"}
+--          , ProperName {runProperName = "Eff"}
+--          , ProperName {runProperName = "Console"}
+--          ])
+--       Implicit
+--       Nothing
+--   , ValueDeclaration
+--       (ValueDeclarationData
+--        { valdeclSourceAnn =
+--            ( SourceSpan
+--              { spanName = "file.purs"
+--              , spanStart = SourcePos {sourcePosLine = 5, sourcePosColumn = 1}
+--              , spanEnd = SourcePos {sourcePosLine = 5, sourcePosColumn = 12}
+--              }
+--            , [])
+--        , valdeclIdent = Ident "main"
+--        , valdeclName = Public
+--        , valdeclBinders = []
+--        , valdeclExpression =
+--            [ GuardedExpr
+--                []
+--                (PositionedValue
+--                   (SourceSpan
+--                    { spanName = "file.purs"
+--                    , spanStart =
+--                        SourcePos {sourcePosLine = 5, sourcePosColumn = 8}
+--                    , spanEnd =
+--                        SourcePos {sourcePosLine = 5, sourcePosColumn = 12}
+--                    })
+--                   []
+--                   (PositionedValue
+--                      (SourceSpan
+--                       { spanName = "file.purs"
+--                       , spanStart =
+--                           SourcePos {sourcePosLine = 5, sourcePosColumn = 8}
+--                       , spanEnd =
+--                           SourcePos {sourcePosLine = 5, sourcePosColumn = 12}
+--                       })
+--                      []
+--                      (App
+--                         (PositionedValue
+--                            (SourceSpan
+--                             { spanName = "file.purs"
+--                             , spanStart =
+--                                 SourcePos
+--                                 {sourcePosLine = 5, sourcePosColumn = 8}
+--                             , spanEnd =
+--                                 SourcePos
+--                                 {sourcePosLine = 5, sourcePosColumn = 11}
+--                             })
+--                            []
+--                            (Var (Qualified Nothing (Ident "log"))))
+--                         (PositionedValue
+--                            (SourceSpan
+--                             { spanName = "file.purs"
+--                             , spanStart =
+--                                 SourcePos
+--                                 {sourcePosLine = 5, sourcePosColumn = 12}
+--                             , spanEnd =
+--                                 SourcePos
+--                                 {sourcePosLine = 5, sourcePosColumn = 12}
+--                             })
+--                            []
+--                            (Literal (StringLiteral "Hello, World!"))))))
+--            ]
+--        })
+--   ]
+--   Nothing
---
+++ test/ParserExplorationSpec.hs
@@ -0,0 +1,59 @@
+module ParserExplorationSpec
+  ( main
+  , spec
+  ) where
+
+import Protolude
+import Test.Hspec
+import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+
+import Control.Arrow ((>>>))
+import Data.String.Here
+import qualified Language.PureScript as PS
+import qualified Text.Parsec as P (ParseError)
+
+main :: IO ()
+main = hspecWith defaultConfig {configFastFail = True} spec
+
+tokenize :: Text -> Either P.ParseError [Text]
+tokenize = PS.lex "file.purs" >>> fmap (map show)
+
+parse :: Text -> Either P.ParseError PS.Module
+parse source =
+  PS.lex "file.purs" source >>= PS.runTokenParser "file.purs" PS.parseModule
+
+spec :: Spec
+spec = do
+  let source =
+        [here|
+module Main where
+
+import Control.Monad.Eff.Console
+
+main = log "Hello, World!"
+|]
+  describe "purescript parser exploration" $ do
+    it "lexes" $
+      tokenize source `shouldBe`
+      Right
+        [ "\"module\""
+        , "\"Main\""
+        , "\"where\""
+        , "\"import\""
+        , "qualifier"
+        , "qualifier"
+        , "qualifier"
+        , "\"Console\""
+        , "\"main\""
+        , "="
+        , "\"log\""
+        , "\"Hello, World!\""
+        ]
+    it "parses" $ do
+      let (Right mod) = parse source
+      let (PS.Module srcSpan comments modName declarations declarRefs) = mod
+      -- srcSpan `shouldBe` undefined
+      comments `shouldBe` []
+      -- modName `shouldBe` undefined
+      -- declarations `shouldBe` undefined -- We don't have Eq here
+      declarRefs `shouldBe` []
