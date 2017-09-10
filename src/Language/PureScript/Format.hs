6d27cf0b0ce6edf53c9c6701643d1b488b713eb2
Louis Pilfold <louis@lpil.uk>
2017-09-10 12:36:01 +0100

    Remove use of Pretty typeclass

--- src/Language/PureScript/Format.hs
+++ src/Language/PureScript/Format.hs
@@ -9,6 +9,8 @@ import Control.Category ((>>>))
 import qualified Language.PureScript as PS
 import Protolude
 import qualified Text.Parsec as Parsec
+
+-- https://hackage.haskell.org/package/wl-pprint-text-1.1.1.0/docs/Text-PrettyPrint-Leijen-Text.html
 import Text.PrettyPrint.Leijen.Text as PrettyPrint
 
 data Options = Options
@@ -16,9 +18,6 @@ data Options = Options
   , filename :: FilePath
   }
 
-{- Hey, take this. It will come in handy.
-   https://hackage.haskell.org/package/wl-pprint-text-1.1.1.0/docs/Text-PrettyPrint-Leijen-Text.html
--}
 {-| Left to right function application.
 -}
 (|>) :: a -> (a -> c) -> c
@@ -42,70 +41,56 @@ format = do
 formatWith :: Options -> Text -> Either Parsec.ParseError Text
 formatWith options source = do
   module' <- parse (filename options) source
-  Module module' |> pretty |> renderPretty 0.9 (maxWidth options) |>
-    displayTStrict |>
-    pure
+  pure (formatRender module')
+  where
+    formatRender =
+      prettyModule >>> renderPretty 0.9 (maxWidth options) >>> displayTStrict
 
-{-| A Module of PS code.
--}
-newtype Module =
-  Module PS.Module
-
-instance Pretty Module where
-  pretty (Module (PS.Module _ _ moduleName declarations _)) =
-    mconcat [prettyModuleHeader moduleName, prettyDeclarations declarations]
+prettyModule :: PS.Module -> Doc
+prettyModule (PS.Module _ _ moduleName declarations _) =
+  mconcat [prettyModuleHeader moduleName, prettyDeclarations declarations]
 
 prettyModuleHeader :: PS.ModuleName -> Doc
-prettyModuleHeader name =
-  mconcat ["module ", pretty (ModuleName name), " where"]
+prettyModuleHeader name = mconcat ["module ", prettyModuleName name, " where"]
+
+prettyModuleName :: PS.ModuleName -> Doc
+prettyModuleName (PS.ModuleName names) =
+  let nameDoc = PS.runProperName >>> fromStrict >>> text
+  in names |> map nameDoc |> intersperse dot |> mconcat
 
 prettyDeclarations :: [PS.Declaration] -> Doc
 prettyDeclarations [] = mempty
 prettyDeclarations declarations =
   let declarationDocs =
-        declarations |> map (Declaration >>> pretty) |> intersperse linebreak
+        declarations |> map prettyDeclaration |> intersperse linebreak
   in mconcat $ linebreak : linebreak : declarationDocs
 
-{-| The module statement, module name, and exports.
--}
-newtype ModuleName =
-  ModuleName PS.ModuleName
-
-instance Pretty ModuleName where
-  pretty (ModuleName (PS.ModuleName names)) =
-    let nameDoc = PS.runProperName >>> fromStrict >>> text
-    in names |> map nameDoc |> intersperse dot |> mconcat
-
 {-| A top level declaration, such as an import or a function definition.
--}
-newtype Declaration =
-  Declaration PS.Declaration
 
--- purescript/src/Language/PureScript/AST/Declarations.hs
-instance Pretty Declaration where
-  pretty (Declaration dec) =
-    case dec of
-      PS.ImportDeclaration _ name type' qualifier ->
-        prettyImport name type' qualifier
-      PS.DataDeclaration _ _ _ _ _ -> undefined
-      PS.DataBindingGroupDeclaration _ -> undefined
-      PS.TypeSynonymDeclaration _ _ _ _ -> undefined
-      PS.TypeDeclaration _ -> undefined
-      PS.ValueDeclaration _ -> undefined
-      PS.BoundValueDeclaration _ _ _ -> undefined
-      PS.BindingGroupDeclaration _ -> undefined
-      PS.ExternDeclaration _ _ _ -> undefined
-      PS.ExternDataDeclaration _ _ _ -> undefined
-      PS.ExternKindDeclaration _ _ -> undefined
-      PS.FixityDeclaration _ _ -> undefined
-      PS.TypeClassDeclaration _ _ _ _ _ _ -> undefined
-      PS.TypeInstanceDeclaration _ _ _ _ _ _ -> undefined
+    purescript/src/Language/PureScript/AST/Declarations.hs
+-}
+prettyDeclaration :: PS.Declaration -> Doc
+prettyDeclaration (PS.ImportDeclaration _ name type' qualifier) =
+  prettyImport name type' qualifier
+prettyDeclaration (PS.DataDeclaration _ _ _ _ _) = undefined
+prettyDeclaration (PS.DataBindingGroupDeclaration _) = undefined
+prettyDeclaration (PS.TypeSynonymDeclaration _ _ _ _) = undefined
+prettyDeclaration (PS.TypeDeclaration _) = undefined
+prettyDeclaration (PS.ValueDeclaration _) = undefined
+prettyDeclaration (PS.BoundValueDeclaration _ _ _) = undefined
+prettyDeclaration (PS.BindingGroupDeclaration _) = undefined
+prettyDeclaration (PS.ExternDeclaration _ _ _) = undefined
+prettyDeclaration (PS.ExternDataDeclaration _ _ _) = undefined
+prettyDeclaration (PS.ExternKindDeclaration _ _) = undefined
+prettyDeclaration (PS.FixityDeclaration _ _) = undefined
+prettyDeclaration (PS.TypeClassDeclaration _ _ _ _ _ _) = undefined
+prettyDeclaration (PS.TypeInstanceDeclaration _ _ _ _ _ _) = undefined
 
 prettyImport :: PS.ModuleName
              -> PS.ImportDeclarationType
              -> Maybe PS.ModuleName
              -> Doc
-prettyImport name PS.Implicit Nothing = "import " <> pretty (ModuleName name)
+prettyImport name PS.Implicit Nothing = "import " <> prettyModuleName name
 prettyImport _ _ _ = undefined
 
 {-| Parse a module of PS source code.
