b098885ad6a171e7231f112ba5c6f140818f7f0f
Louis Pilfold <louis@lpil.uk>
2017-12-22 23:18:44 +0000

    Brittany format

--- src/Language/PureScript/Format.hs
+++ src/Language/PureScript/Format.hs
@@ -42,9 +42,9 @@ formatWith :: Options -> Text -> Either Parsec.ParseError Text
 formatWith options source = do
   module' <- parse (filename options) source
   pure (formatRender module')
-  where
-    formatRender =
-      prettyModule >>> renderPretty 0.9 (maxWidth options) >>> displayTStrict
+ where
+  formatRender =
+    prettyModule >>> renderPretty 0.9 (maxWidth options) >>> displayTStrict
 
 prettyModule :: PS.Module -> Doc
 prettyModule (PS.Module _ _ moduleName declarations _) =
@@ -65,7 +65,7 @@ prettyDeclarations [] = mempty
 prettyDeclarations declarations =
   let declarationDocs =
         declarations |> map prettyDeclaration |> intersperse linebreak
-  in mconcat $ linebreak : linebreak : declarationDocs
+  in  mconcat $ linebreak : linebreak : declarationDocs
 
 {-| A top level declaration, such as an import or a function definition.
 
@@ -74,34 +74,33 @@ purescript/src/Language/PureScript/AST/Declarations.hs
 prettyDeclaration :: PS.Declaration -> Doc
 prettyDeclaration (PS.ImportDeclaration _ name type' qualifier) =
   prettyImport name type' qualifier
-prettyDeclaration x@(PS.DataDeclaration _ _ _ _ _) = unimplemented x
-prettyDeclaration x@(PS.DataBindingGroupDeclaration _) = unimplemented x
-prettyDeclaration x@(PS.TypeSynonymDeclaration _ _ _ _) = unimplemented x
-prettyDeclaration x@(PS.TypeDeclaration _) = unimplemented x
-prettyDeclaration x@(PS.ValueDeclaration _) = unimplemented x
-prettyDeclaration x@(PS.BoundValueDeclaration _ _ _) = unimplemented x
-prettyDeclaration x@(PS.BindingGroupDeclaration _) = unimplemented x
-prettyDeclaration x@(PS.ExternDeclaration _ _ _) = unimplemented x
-prettyDeclaration x@(PS.ExternDataDeclaration _ _ _) = unimplemented x
-prettyDeclaration x@(PS.ExternKindDeclaration _ _) = unimplemented x
-prettyDeclaration x@(PS.FixityDeclaration _ _) = unimplemented x
-prettyDeclaration x@(PS.TypeClassDeclaration _ _ _ _ _ _) = unimplemented x
+prettyDeclaration x@(PS.DataDeclaration _ _ _ _ _          ) = unimplemented x
+prettyDeclaration x@(PS.DataBindingGroupDeclaration _      ) = unimplemented x
+prettyDeclaration x@(PS.TypeSynonymDeclaration _ _ _ _     ) = unimplemented x
+prettyDeclaration x@(PS.TypeDeclaration  _                 ) = unimplemented x
+prettyDeclaration x@(PS.ValueDeclaration _                 ) = unimplemented x
+prettyDeclaration x@(PS.BoundValueDeclaration _ _ _        ) = unimplemented x
+prettyDeclaration x@(PS.BindingGroupDeclaration _          ) = unimplemented x
+prettyDeclaration x@(PS.ExternDeclaration     _ _ _        ) = unimplemented x
+prettyDeclaration x@(PS.ExternDataDeclaration _ _ _        ) = unimplemented x
+prettyDeclaration x@(PS.ExternKindDeclaration _ _          ) = unimplemented x
+prettyDeclaration x@(PS.FixityDeclaration     _ _          ) = unimplemented x
+prettyDeclaration x@(PS.TypeClassDeclaration    _ _ _ _ _ _) = unimplemented x
 prettyDeclaration x@(PS.TypeInstanceDeclaration _ _ _ _ _ _) = unimplemented x
 
-prettyImport :: PS.ModuleName
-             -> PS.ImportDeclarationType
-             -> Maybe PS.ModuleName
-             -> Doc
+prettyImport
+  :: PS.ModuleName -> PS.ImportDeclarationType -> Maybe PS.ModuleName -> Doc
 prettyImport name PS.Implicit Nothing = "import " <> prettyModuleName name
 prettyImport name (PS.Explicit defRefs) Nothing =
   let header = prettyImport name PS.Implicit Nothing
       refsDoc =
-        map prettyDeclarationRef defRefs |> intersperse ("," <> softline) |>
-        mconcat |>
-        nest 8 |>
-        group |>
-        parens
-  in (header <> softline |> nest 7 |> group) <> refsDoc |> group
+        map prettyDeclarationRef defRefs
+          |> intersperse ("," <> softline)
+          |> mconcat
+          |> nest 8
+          |> group
+          |> parens
+  in  (header <> softline |> nest 7 |> group) <> refsDoc |> group
 prettyImport a b c = unimplemented (a, b, c)
 
 prettyDeclarationRef :: PS.DeclarationRef -> Doc
@@ -111,25 +110,27 @@ prettyDeclarationRef (PS.ValueOpRef _ (PS.OpName ident)) =
   ident |> fromStrict |> text |> parens
 prettyDeclarationRef (PS.TypeRef _ name (Just [])) = prettyProperName name
 prettyDeclarationRef (PS.TypeRef _ name (Just names)) =
-  prettyProperName name <>
-  (map prettyProperName names |> intersperse ("," <> space) |> mconcat |> parens)
-prettyDeclarationRef x@(PS.TypeRef _ _ _) = unimplemented x
-prettyDeclarationRef x@(PS.TypeOpRef _ _) = unimplemented x
-prettyDeclarationRef x@(PS.TypeClassRef _ _) = unimplemented x
-prettyDeclarationRef x@(PS.ModuleRef _ _) = unimplemented x
+  prettyProperName name
+    <> (  map prettyProperName names
+       |> intersperse ("," <> space)
+       |> mconcat
+       |> parens
+       )
+prettyDeclarationRef x@(PS.TypeRef _ _ _      ) = unimplemented x
+prettyDeclarationRef x@(PS.TypeOpRef       _ _) = unimplemented x
+prettyDeclarationRef x@(PS.TypeClassRef    _ _) = unimplemented x
+prettyDeclarationRef x@(PS.ModuleRef       _ _) = unimplemented x
 prettyDeclarationRef x@(PS.TypeInstanceRef _ _) = unimplemented x
-prettyDeclarationRef x@(PS.KindRef _ _) = unimplemented x
-prettyDeclarationRef x@(PS.ReExportRef _ _ _) = unimplemented x
+prettyDeclarationRef x@(PS.KindRef         _ _) = unimplemented x
+prettyDeclarationRef x@(PS.ReExportRef _ _ _  ) = unimplemented x
 
 {-| Parse a module of PS source code.
 -}
 parse :: FilePath -> Text -> Either Parsec.ParseError PS.Module
 parse path source = tokenize path source >>= parseTokens path
-  where
-    tokenize filepath = PS.lex filepath
-    parseTokens filepath = PS.runTokenParser filepath PS.parseModule
+ where
+  tokenize filepath = PS.lex filepath
+  parseTokens filepath = PS.runTokenParser filepath PS.parseModule
 
-unimplemented
-  :: Show a
-  => a -> b
+unimplemented :: Show a => a -> b
 unimplemented x = error ("function clause not implemented for " <> show x)
